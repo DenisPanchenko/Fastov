d142637a88f5b58c97bbd21592956094a8ce79b0
Tanya <tanya.pushchalo@gmail.com>
2013-10-30 23:33:46 +0200

    message

--- src/core/DBManager.java
+++ src/core/DBManager.java
@@ -13,7 +13,6 @@ package core;
 import java.text.SimpleDateFormat;
 import java.util.*;
 import java.io.*;
-import java.nio.file.Files;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
