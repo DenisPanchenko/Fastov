d07b310c5d68846b385a8cece1b96d12fa5e23fa
Tanya <tanya.pushchalo@gmail.com>
2013-11-02 17:06:08 +0200

    m

--- src/gui/MainForm.java
+++ src/gui/MainForm.java
@@ -1,32 +1,18 @@
 package gui;
 
 import java.awt.BorderLayout;
-import java.awt.Dialog.ModalityType;
 import java.awt.EventQueue;
-import java.awt.FlowLayout;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
-import javax.swing.table.TableColumn;
-import javax.swing.tree.DefaultMutableTreeNode;
 import javax.swing.tree.DefaultTreeModel;
-import javax.swing.JComboBox;
-import javax.swing.JDialog;
-import javax.swing.JLabel;
-import javax.swing.JTextField;
 import javax.swing.JTree;
 import javax.swing.JTable;
 import javax.swing.JButton;
 
 import core.DBManager;
-import core.DataBase;
-import core.DataType;
-import core.DataType.TYPE;
-
 import java.awt.GridLayout;
-import java.util.ArrayList;
-import java.util.List;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
